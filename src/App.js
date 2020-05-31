2e31d14e3721127c55204f5a7bfee2cf52922035
Jiaxi <dev.jiaxiwu@gmail.com>
2020-05-31 17:19:21 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component } from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
@@ -6,19 +6,29 @@ import Shop from "./components/Shop";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/About";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/shop" component={Shop} />
-          <Route path="/about" component={About} />
-        </Switch>
+{/* <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/shop" component={Shop} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router> */} 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6"> column number one </div>
+          <div className="col-6"> column number one </div>
+          <div className="col-6"> column number one </div>
+        </div>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,21 @@
 import React from "react";
+import "../../src/components/style/about.css";
 
 function About() {
   return (
     <div>
       <section>
-        <div className="container">
-          <div class="col-12 col-sm-8 col-lg-6 col-xl-5 text-center">
-            <h6>Hello, we are</h6>
-            <div class="special-hdr-xl">Stick With Me</div>
-            <p>
-              We are a team of chocolatiers who make exquisite confections by
-              hand in New York City. We believe in hard work, fresh and natural
-              ingredients, and that sweets make the world go ‘round.
-            </p>
+        <div id="about-main">
+          <div className="container">
+            <div class="col-12 col-sm-8 col-lg-6 col-xl-5 text-center">
+              <h6>Hello, we are</h6>
+              <div class="special-hdr-xl">Stick With Me</div>
+              <p>
+                We are a team of chocolatiers who make exquisite confections by
+                hand in New York City. We believe in hard work, fresh and
+                natural ingredients, and that sweets make the world go ‘round.
+              </p>
+            </div>
           </div>
         </div>
       </section>
