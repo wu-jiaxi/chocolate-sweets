8498dc462ffa7d3c902c13b69bf280069549c1bd
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-02 03:36:34 -0400

    price tag

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 /* import Home from "./components/Home";
 import Header from "./components/Header";
@@ -30,11 +31,12 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={ProductList}/>
-          <Route path="/details" component={Details}/>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
   }
