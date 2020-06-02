d037fd3879d101ad683cf2e241f0d59f2ad2ca21
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-02 06:04:43 -0400

    cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -24,12 +24,25 @@ export default function CartItem({ item, value }) {
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
           <div>
-            <span
-              className="btn btn-black mx-1"
-              onClick={() => decrement(id)}
-            ></span>
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+              {" "}
+              -{" "}
+            </span>
+            <span className="btn btn-black mx-1"> {count}</span>
+            <span className="btn btn-black mx-1" onClick={() => increment(id)}>
+              {" "}
+              +
+            </span>
           </div>
         </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash"></i>
+        </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <strong> item total : ${total}</strong>
       </div>
     </div>
   );
---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function CartTotals({ value }) {
+  const { cartSubtotal, carTax, carTotal, clearCart } = value;
+  return <div></div>;
+}
