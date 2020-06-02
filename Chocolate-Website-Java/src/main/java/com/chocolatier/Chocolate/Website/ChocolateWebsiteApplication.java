a6b3dbf87e2f4594850945c655a98ea6ff307736
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-02 08:34:40 -0400

    java

--- Chocolate-Website-Java/src/main/java/com/chocolatier/Chocolate/Website/ChocolateWebsiteApplication.java
+++ Chocolate-Website-Java/src/main/java/com/chocolatier/Chocolate/Website/ChocolateWebsiteApplication.java
@@ -1,13 +1,13 @@
-package com.pusher.shoppingcart;
+package com.chocolatier.Chocolate.Website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingCartApplication {
+public class ChocolateWebsiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingCartApplication.class, args);
+		SpringApplication.run(ChocolateWebsiteApplication.class, args);
 	}
 
 }
--- Chocolate-Website-Java/src/test/java/com/chocolatier/Chocolate/Website/ChocolateWebsiteApplicationTests.java
+++ Chocolate-Website-Java/src/test/java/com/chocolatier/Chocolate/Website/ChocolateWebsiteApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pusher.shoppingcart;
+package com.chocolatier.Chocolate.Website;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppingCartApplicationTests {
+class ChocolateWebsiteApplicationTests {
 
 	@Test
 	void contextLoads() {
