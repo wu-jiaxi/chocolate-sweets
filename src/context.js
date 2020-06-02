ad418a757e2f8415dca0723d5c142bbc971dbe3b
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-02 06:22:22 -0400

    addtotals function

--- src/context.js
+++ src/context.js
@@ -7,7 +7,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: storeProducts,
+    cart: [],
     modalOpen: false,
     modalProduct: detailProduct,
     cartSubTotal: 0,
@@ -90,6 +90,21 @@ class ProductProvider extends Component {
 
   clearCart = () => {
     console.log("cart was cleared");
+  };
+
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map((item) => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total,
+      };
+    });
   };
   render() {
     return (
