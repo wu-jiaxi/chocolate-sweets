8304a19770e972d10e1175bc52af1c4ebdc0c0e3
Jiaxi <dev.jiaxiwu@gmail.com>
2020-07-02 19:52:31 -0400

    css

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,20 +5,15 @@ import ReactPlayer from "react-player";
 
 function MainSection() {
   return (
-    <div className="main-section .container">
-      <img
-        src={mainpicturebonbon}
-        width="100%"
-        className="clsFirstImg"
-        alt="bonbon"
-      ></img>
+    <div id="main-container" className="container">
+      {/* <img src={mainpicturebonbon} className="clsFirstImg" alt="bonbon"></img> */}
       <div className="clsSecondImg">
-        <div className="bonBonVideo">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=U7cUzRpoDRE"
-            width="800px"
-            height="500px"
-          />
+        <div className="content">
+          <iframe
+            frameborder="0"
+            allowfullscreen=""
+            src="http://www.youtube.com/embed/_OBlgSz8sSM"
+          ></iframe>
         </div>
       </div>
     </div>
