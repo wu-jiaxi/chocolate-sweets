4268b1d11f26fe7e8b65d051065419fa9bf9e602
Jiaxi <dev.jiaxiwu@gmail.com>
2020-07-06 12:36:51 -0400

    cover

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./style/mainsection.css";
-import mainpicturebonbon from "/Users/jiaxi/Desktop/Perscholas/hw/ecommerce/ecommerce/src/components/assets/mainpicbonbon.jpg";
-import ReactPlayer from "react-player";
 
 function MainSection() {
   return (
-    <div>
+    <div className="all-wrap">
       {/* <img src={mainpicturebonbon} className="clsFirstImg" alt="bonbon"></img> */}
       <div className="container">
         <div className="video-container">
@@ -15,7 +13,6 @@ function MainSection() {
             src="https://www.youtube.com/embed/U7cUzRpoDRE"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
           ></iframe>
         </div>
       </div>
