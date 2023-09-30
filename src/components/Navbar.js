e9b161bcb133cc0e6f839153b709bad2af11dce4
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-30 14:05:13 -0500

    fixed stylistic bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
+import "./style/header.css";
 
 class Navbar extends Component {
   render() {
     return (
-      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <NavWrapper
+        className="navbar navbar-expand-sm navbar-dark px-sm-5"
+        id="navbarFlex"
+      >
         {/* 
 https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
                 https://www.iconfinder.com/Makoto_msk */}
 
         <Link to="/">
-          <h2 className="navbar-brand">Stick with me Sweets</h2>
+          <h2 className="webTitle">Stick with me Sweets</h2>
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="/products" className="nav-link">
-              products
+            <Link to="/products" className="products">
+              Products
             </Link>
           </li>
         </ul>
@@ -36,7 +40,7 @@ Creative Commons (Attribution 3.0 Unported);
 }
 
 const NavWrapper = styled.nav`
-  background: #f6d8e0;
+  background: #d9fff8;
   .nav-link {
     color: var(--mainWhite) !important;
     font-size: 1.3rem;
